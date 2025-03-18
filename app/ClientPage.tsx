33643ae652123c54e2b9bad9d20e5f4ad4460a12
i9placesolutions <rafael@i9place.com.br>
2025-03-17 22:26:07 -0300

    fix: aumenta ainda mais a área azul (40rem e -300px)

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -286,7 +286,7 @@ export default function ClientPage() {
       {/* Hero Section */}
       <section className="relative bg-[#01294C] text-white py-16 md:py-24 overflow-hidden">
         {/* Ondas decorativas - posicionada mais abaixo */}
-        <div className="absolute bottom-[-200px] left-0 right-0">
+        <div className="absolute bottom-[-300px] left-0 right-0">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full h-auto">
             <path fill="#ffffff" fillOpacity="1" d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,112C672,96,768,96,864,112C960,128,1056,160,1152,160C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
